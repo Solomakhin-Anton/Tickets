f7fdf9ec969a25abb1b0ac3a4d672231f29028ae
Solomakhin Anton <s.anton45@yandex.ru>
2020-04-12 14:06:44 +0300

    Comparator branch edited

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -1,26 +1,29 @@
-package ru.netology.domain;
+package ru.netology.manager;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.netology.domain.Ticket;
+import ru.netology.domain.TicketByPriceAscComparator;
+import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Manager {
-    Repository list;
+public class TicketManager {
+    TicketRepository list;
+
     public Ticket[] findAll(String from, String to, TicketByPriceAscComparator comparator) {
         Ticket[] newList = new Ticket[0];
         for (Ticket ticket : list.getAll()) {
-            if (ticket.arrival.equals(to) && ticket.departure.equals(from)) {
+            if (ticket.getArrival().equals(to) && ticket.getDeparture().equals(from)) {
                 Ticket[] tmp = new Ticket[newList.length + 1];
                 System.arraycopy(newList, 0, tmp, 0, newList.length);
                 tmp[tmp.length - 1] = ticket;
                 newList = tmp;
                 Arrays.sort(newList, comparator);
-                System.out.println(ticket);
             }
 
         }
--- src/test/java/ru/netology/domain/ManagerTest.java
+++ src/test/java/ru/netology/domain/ManagerTest.java
@@ -1,18 +1,20 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
+import ru.netology.manager.TicketManager;
+import ru.netology.repository.TicketRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerTest {
-    Repository repo = new Repository();
-    Manager manager = new Manager(repo);
+    TicketRepository repo = new TicketRepository();
+    TicketManager manager = new TicketManager(repo);
 
     @Test
     void shouldFindAll() {
         Ticket[] expected = {
                 new Ticket(10, 4114, "SVO", "KZN", 85),
-                new Ticket(1, 1299,"SVO", "KZN", 95),
+                new Ticket(1, 1299, "SVO", "KZN", 95),
                 new Ticket(9, 4114, "SVO", "KZN", 100)};
         Ticket[] actual = manager.findAll("KZN", "SVO", new TicketByPriceAscComparator());
         assertArrayEquals(expected, actual);
